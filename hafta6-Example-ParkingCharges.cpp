59d5f546b77e31176f53f727ba3207f2efa19105
ozanba <70808189+ozanba@users.noreply.github.com>
2023-11-06 21:49:13 +0300

    Update hafta6-Example-ParkingCharges.cpp

--- hafta6-Example-ParkingCharges.cpp
+++ hafta6-Example-ParkingCharges.cpp
@@ -4,12 +4,17 @@ using namespace std;
 int main(){
     const int limit = 3;
     int hour[limit];
+    int totalHour = 0;
 
     for(int i = 0; i<limit; i++){
         cout<<"Enter hours for "<<(i+1)<<". Customer: ";
         cin>>hour[i];
+        totalHour +=hour[i];
     }
+    // baş satır -- head line --
     cout << setw(5) << "Car" << setw(15) << "Hours" << setw(15) << "Charge\n";
+    // orta satırların charge değerininin hesaplanması ve printlenmesi.
+    double totalCharge = 0;
     for(int i= 0;i<limit;i++){
         double charge = 0.00;
         if(hour[i]<3)
@@ -17,9 +22,16 @@ int main(){
         else{
             charge =  (hour[i]-3)*0.5 + 2;
         }
-        //setw tbın havalı versiyonudur.
+        totalCharge += charge;
+        //setw tab!ın havalı versiyonudur. width yani genişliği belirler.
         cout<<setw(3)<<(i+1)<<setw(17)<<hour[i]<<setw(14)<<fixed<<setprecision(2)<<charge<<endl;
     }
+
+
+
+    // son satır, Toplam satırı
+
+    cout<<setw(5)<<"TOPLAM"<<setw(14)<<totalHour<<setw(14)<<fixed<<setprecision(2)<<totalCharge<<endl;
 
     return 0;
     }
